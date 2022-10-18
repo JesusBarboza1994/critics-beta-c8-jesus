c25d0b0c96d1d93b0705d70ebe565cae5718f29e
jesus <jbarz20@gmail.com>
2022-10-18 10:57:54 -0500

    ADD: authenticate edit method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action: authenticate_user except: i%Ñ[index show]
   helper_method %i[current_user logged_in?]
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
@@ -17,4 +18,12 @@ class ApplicationController < ActionController::Base
     session.delete(:user_id)
     flash[:info] = "Thanks for using Critics beta"
   end
+
+  def authenticate_user
+    if logged_in?
+      nil
+    else
+      flash[:message] = "You need to be logged in to perform this action"
+      redirecto_to login_path
+    end
 end
